06da262b95e7ddb6f47d12ff60432563d07c4df1
David Orozco <orozco.davidj02@gmail.com>
2022-03-22 10:16:47 -0500

    feat: update app example to match with new insertion api

--- app/src/main/java/EDT/App.java
+++ app/src/main/java/EDT/App.java
@@ -6,24 +6,27 @@ public class App {
     public static void main(String[] args) {
         NAryTree tree = new NAryTree();
         tree.setTitle("This is the title");
-        tree.insert("This is the title", "Node A");
-        tree.insert("This is the title", "Node B");
-        tree.insert("Node A", "Node A.1");
+        tree.insert("This is the title", "Node A", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("This is the title", "Node B", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node A", "Node A.1", NAryTree.NodeType.PACKAGE_NODE);
 
 
-        tree.insert("Node B", "Test");
+        tree.insert("Node B", "Test", NAryTree.NodeType.PACKAGE_NODE);
 
-        tree.insert("Node C", "Lol");
-        tree.insert("This is the title", "Node C");
-        tree.insert("Node C", "Lol");
-        tree.insert("Node A.1", "Node D");
-        tree.insert("Node A.1", "Node D.1");
-        tree.insert("Node A.1", "Node D.4");
-        tree.insert("Node D", "Node E");
-        tree.insert("Node E", "Node F");
-        tree.insert("Node G", "Node F");
-        tree.insert("Node A.1", "Node D.3");
+        tree.insert("Node C", "Lol", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("This is the title", "Node C", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node C", "Lol", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node A.1", "Node D", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node A.1", "Node D.1", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node A.1", "Node D.4", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node D", "Node E", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node E", "Node F", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node G", "Node F", NAryTree.NodeType.PACKAGE_NODE);
+        tree.insert("Node A.1", "Node D.3", NAryTree.NodeType.DELIVERABLE_NODE);
+        tree.insert("Node A", "Node A.52 project", NAryTree.NodeType.PACKAGE_NODE);
 
+        tree.insertDerivableNode("Node A", "Final node", "This is the content of the file");
+        tree.insertDerivableNode("Node A", "this is a try", "this is another file");
         System.out.println(tree);
     }
 }
