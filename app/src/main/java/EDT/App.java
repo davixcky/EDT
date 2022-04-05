27655f804e06313521939dc39dfe2ce8cfdcc9f8
David Orozco <orozco.davidj02@gmail.com>
2022-04-05 09:47:03 -0500

    feat: update app name

--- app/src/main/java/EDT/App.java
+++ app/src/main/java/EDT/App.java
@@ -4,7 +4,7 @@ import EDT.UI.MainDisplay;
 
 public class App {
     public static void main(String[] args) {
-        MainDisplay mainDisplay = new MainDisplay("test");
+        MainDisplay mainDisplay = new MainDisplay("Project Management Office");
 
         mainDisplay.show();
 
