8aebd6cb16955c5b187dfafa374f594829d1f7de
David Orozco <orozco.davidj02@gmail.com>
2022-04-23 12:43:36 -0500

    feat: add main input managers

---
+++ app/src/main/java/EDT/input/KeyManager.java
@@ -0,0 +1,95 @@
+package EDT.input;
+
+import EDT.UI.UIManager;
+import EDT.services.data.LinkedList;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+
+public class KeyManager implements KeyListener {
+
+    private HashMap<String, LinkedList<KeyListener>> keyListeners;
+    private boolean keys[];
+    private UIManager uiManager;
+
+    public KeyManager() {
+        keyListeners = new HashMap<>();
+        keys = new boolean[256];
+    }
+
+    public void setUIManager(UIManager uiManager) {
+        this.uiManager = uiManager;
+    }
+
+    public void update() {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyCode() >= 256 || e.getKeyCode() < 0)
+            return;
+
+        LinkedList<KeyListener> listeners = keyListeners.get(Integer.toString(e.getKeyCode()));
+        if (listeners != null) {
+            for (KeyListener l : listeners) {
+                l.typed();
+            }
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() >= 256 || e.getKeyCode() < 0)
+            return;
+
+        LinkedList<KeyListener> listeners = keyListeners.get(Integer.toString(e.getKeyCode()));
+        if (listeners != null) {
+            for (KeyListener l : listeners) {
+                l.pressed();
+            }
+        }
+
+        keys[e.getKeyCode()] = true;
+        if (uiManager != null)
+            uiManager.onKeyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() >= 256 || e.getKeyCode() < 0)
+            return;
+
+        LinkedList<KeyListener> listeners = keyListeners.get(Integer.toString(e.getKeyCode()));
+        if (listeners != null) {
+            for (KeyListener l : listeners) {
+                l.released();
+            }
+        }
+
+        keys[e.getKeyCode()] = false;
+    }
+
+    public void addKeyListener(int keyCode, KeyListener l) {
+        String key = Integer.toString(keyCode);
+
+        if (!keyListeners.containsKey(key)) {
+            keyListeners.put(key, new LinkedList<>());
+        }
+
+        LinkedList<KeyListener> listeners = keyListeners.get(key);
+        listeners.add(l);
+    }
+
+    public boolean getStatusKey(int keyCode) {
+        return keys[keyCode];
+    }
+
+    public interface KeyListener {
+        void pressed();
+
+        void typed();
+
+        void released();
+    }
+}
